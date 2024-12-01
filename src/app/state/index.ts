d3aae97f3b3f0ab0202145e1ac91d59eb72285f8
ibouelezz <ibrahim.ali.aboelezz@gmail.com>
2024-12-02 01:45:25 +0200

    create zustand store to handle state in a more predictable way, wip

---
+++ src/app/state/index.ts
@@ -0,0 +1,60 @@
+import { create } from 'zustand';
+
+export interface User {
+    userId: string;
+    email: string;
+    tasks: Task[];
+}
+
+export interface Annotation {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    text: string;
+}
+
+export interface Task {
+    taskId: string;
+    assignedTo: string;
+    imageURL: string;
+    annotations: Annotation[];
+    status: string; // later enum
+    createdAt;
+}
+
+interface AppState {
+    users: User[]; // All users
+    tasks: Task[]; // All tasks
+    currentTaskIndex: number; // Index of the current task
+    currentAnnotations: Annotation[]; // Annotations for the current task
+    setTasks: (tasks: Task[]) => void; // Set all tasks
+    getTaskById: (taskId: string) => Task | undefined; // Selector for task by taskId
+    setCurrentTaskIndex: (index: number) => void; // Change task index
+    // setCurrentAnnotations: any;
+    updateAnnotations: (annotations: Annotation[]) => void; // Update current annotations
+}
+
+const useAppStore = create<AppState>((set, get) => ({
+    users: [],
+    tasks: [],
+    currentTaskIndex: 0,
+    currentAnnotations: [],
+    setTasks: (tasks) => set(() => ({ tasks })),
+    getTaskById: (taskId) => {
+        return get().tasks.find((task) => task.taskId === taskId);
+    },
+    setCurrentTaskIndex: (index) =>
+        set((state) => ({
+            currentTaskIndex: index,
+            currentAnnotations: state.tasks[index]?.annotations || [],
+        })),
+    updateAnnotations: (annotations) =>
+        set((state) => {
+            const updatedTasks = [...state.tasks];
+            updatedTasks[state.currentTaskIndex].annotations = annotations;
+            return { tasks: updatedTasks, currentAnnotations: annotations };
+        }),
+}));
+
+export default useAppStore;
