ede6b7fd9212d25dd2c13296a5904b75a627eca3
ibouelezz <ibrahim.ali.aboelezz@gmail.com>
2024-12-03 05:08:16 +0200

    save logic redirect to root tasks page

--- src/app/tasks/[taskId]/page.tsx
+++ src/app/tasks/[taskId]/page.tsx
@@ -107,7 +107,7 @@ const TaskPage = () => {
             {!nextTaskId && (
                 <div className="absolute bottom-6 right-6">
                     <button
-                        onClick={saveAnnotations}
+                        onClick={async () => await saveAnnotations().then(() => redirect('/tasks'))}
                         className="bg-blue-600 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
                     >
                         Save
